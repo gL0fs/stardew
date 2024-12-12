523c3efce441b9a0a5c41ebd0169f914ef86d970
LUOLUO277 <c2483166226@163.com>
2024-12-12 19:07:27 +0800

    更新 MainMenuScene.cpp

--- Classes/MainMenuScene.cpp
+++ Classes/MainMenuScene.cpp
@@ -11,13 +11,37 @@ bool MainMenuScene::init() {
     if (!Scene::init()) return false;
 
     // 设置背景颜色
-    auto background = LayerColor::create(Color4B(50, 150, 255, 255));
-    this->addChild(background);
+    /*auto background = LayerColor::create(Color4B(50, 150, 255, 255));
+    this->addChild(background);*/
+    auto visibleSize = cocos2d::Director::getInstance()->getVisibleSize();
+    cocos2d::Vec2 origin = cocos2d::Director::getInstance()->getVisibleOrigin();
 
+    // 创建一个Layer
+    auto layer = cocos2d::Layer::create();
+
+    // 创建背景Sprite
+    auto background = cocos2d::Sprite::create("stardewPanorama.png");
+
+    // 如果需要，可以设置背景Sprite的位置和大小
+    background->setPosition(origin + visibleSize / 2);
+    background->setScaleX(visibleSize.width / background->getContentSize().width);
+    background->setScaleY(visibleSize.height / background->getContentSize().height);
+
+    // 将背景Sprite添加到Layer中
+    layer->addChild(background);
+
+    // 将Layer添加到场景中
+    this->addChild(layer);
     // 创建标签
-    auto label = Label::createWithTTF("Welcome to Stardew Valley!", "fonts/Marker Felt.ttf", 24);
-    label->setPosition(Director::getInstance()->getVisibleSize() / 2);
-    this->addChild(label);
+   
+  
+    auto sprite = Sprite::create("yellowLettersLogo.png");
+
+    Vec2 spritePosition(origin.x + visibleSize.width / 2, origin.y + visibleSize.height / 2 + visibleSize.height / 4);
+
+    // 设置精灵位置
+    sprite->setPosition(spritePosition);
+    this->addChild(sprite);
 
     // 创建按钮
     auto playButton = MenuItemLabel::create(
