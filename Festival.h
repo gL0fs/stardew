dcff32825c7f1cec96e7bff1d4fafbaa09e4533e
LUOLUO277 <c2483166226@163.com>
2024-12-19 21:38:50 +0800

    festival(first)

---
+++ Festival.h
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef __FESTIVAL_H__
+#define __FESTIVAL_H__
+
+#include "BaseMapLayer.h"
+
+class Festival : public BaseMapLayer
+{
+public:
+    static cocos2d::Scene* createScene();
+    static Festival* create();
+    virtual bool init() override;
+    void initMouseEvent(); // 去掉了 Festival::，因为这是在类定义内部
+    cocos2d::Vec2 getTileCoordForPosition(cocos2d::Vec2 position); // 同上
+    void Festival::switchMap(const std::string& mapName);
+   
+    bool initMap();
+   
+protected:
+    Festival(); // 构造函数
+    virtual ~Festival(); // 析构函数
+    std::map<std::pair<int, int>, int> tileNaijiuMap; // 存储瓷砖坐标和耐久度的映射
+private:
+    cocos2d::EventListenerMouse* _mouseListener;
+    TMXTiledMap* festivalMap; // 将 minemap 重命名为更合适的名称
+   
+};
+
+#endif // __FESTIVAL_H__
