b93f40c04624b5a6b351647979bda5fdf5ced36b
LUOLUO277 <c2483166226@163.com>
2024-12-19 22:10:35 +0800

    mine

--- Classes/Player.h
+++ Classes/Player.h
@@ -2,7 +2,6 @@
 #define PLAYER_H
 
 #include "cocos2d.h"
-#include "Backpack.h"
 #include <vector>
 #include <string>
 
@@ -21,9 +20,8 @@ public:
     void setStamina(int stamina);
     int getStamina() const;
 
-    Inventory* Player::getInventory();
-    void addInventory(const std::string& name, int quantity);
-    void removeInventory(const std::string& name, int quantity);
+    void addItemToInventory(const std::string& item);
+    const std::vector<std::string>& getInventory() const;
 
     void moveUp();
     void moveDown();
@@ -31,14 +29,17 @@ public:
     void moveRight();
 
     Vec2 Player::getxy();//得到玩家当前的瓦片坐标
+
+    int Player::getmoney();//得到当前玩家金钱数量
+    void Player::changemoney(int cost);//改变当前玩家金钱
+
 private:
     Player();
-
+    static int money;
     static Player* _instance;
-
     int _health;
     int _stamina;
-    Inventory* _inventory;
+    std::vector<std::string> _inventory;
 
     CC_DISALLOW_COPY_AND_ASSIGN(Player);
 };
